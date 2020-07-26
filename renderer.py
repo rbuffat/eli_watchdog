11d55fda4f89cce97c5db270388e7645e2be6592
René Buffat <buffat@gmail.com>
2020-07-26 21:39:52 +0200

    sources with no sub category

--- renderer.py
+++ renderer.py
@@ -7,20 +7,26 @@ file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 
 
+def get_country_key(d):
+    if len(d['directory']) == 1:
+        region_key = d['directory'][0]
+        source_key = region_key
+
+    else:
+        region_key = d['directory'][0]
+        source_key = "_".join(d['directory'][1:])
+
+    return {'country': source_key,
+            'region': region_key}
+
+
 def render_menu(data):
     template = env.get_template('menu.html')
 
     r = defaultdict(set)
     for d in data:
-        if len(d['directory']) == 0:
-            region_key = d['directory'][0]
-            source_key = d['directory'][0]
-
-        else:
-            region_key = d['directory'][0]
-            source_key = "_".join(d['directory'][1:])
-
-        r[region_key].add(source_key)
+        country_key = get_country_key(d)
+        r[country_key['region']].add(country_key['country'])
 
     regions = []
     for region in sorted(r):
@@ -35,37 +41,27 @@ def render_menu(data):
 def render_countries(data):
     template = env.get_template('country_sources.html')
 
-    # TODO sources with no sub directory
     collect = defaultdict(list)
     for d in data:
 
-        if len(d['directory']) == 1:
-            region_key = d['directory'][0]
-            source_key = d['directory'][0]
-
-        else:
-            region_key = d['directory'][0]
-            source_key = "_".join(d['directory'][1:])
-
-        country_key = (source_key, region_key)
+        country_key = get_country_key(d)
 
         github_url = 'https://github.com/osmlab/editor-layer-index/tree/gh-pages/sources/{}/{}'.format(
             "/".join(d['directory']),
             d['filename'])
 
         def transform_result(result):
-            print(result)
             result['message'] = html.escape(result['message'])
             return result
 
-        collect[country_key].append({
+        collect[(country_key['region'], country_key['country'])].append({
             'name': d.get('name', 'Not available'),
             'url': github_url,
             'imagery': transform_result(d['imagery']),
             'license_url': transform_result(d['license_url']),
             'privacy_policy_url': transform_result(d['privacy_policy_url'])
         })
-    countries = [{'name': name, 'region': region, 'sources': collect[(name, region)]} for name, region in collect]
+    countries = [{'name': name, 'region': region, 'sources': collect[(region, name)]} for region, name in collect]
 
     return template.render(countries=countries)
 
