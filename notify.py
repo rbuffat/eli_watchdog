c0ec366be6ee6cd6518613d679e993c1aaea80af
René Buffat <buffat@gmail.com>
2021-06-07 22:13:27 +0200

    close issues when imagery is working again

--- notify.py
+++ notify.py
@@ -1,15 +1,19 @@
+import datetime
 import json
 import os
+import re
 
-from github.Repository import Repository
-from github import Github
 import dateutil.parser
-import datetime
-
+from github import Github
+from github.Issue import Issue
+from github.Repository import Repository
 
 GITHUB_API_URL = "https://api.github.com"
 GITHUB_REPO = "osmlab/editor-layer-index"
 CREATE_ISSUE_AFTER_DAYS = 5
+
+ISSUE_OPEN = "open"
+ISSUE_CLOSED = "closed"
 
 
 def query_contributors(repo: Repository, filepath: str) -> set[str]:
@@ -17,13 +21,15 @@ def query_contributors(repo: Repository, filepath: str) -> set[str]:
 
     Parameters
     ----------
+    repo : Repository
+        The repository to query
     filepath : str
         Path in ELI that should be queried
 
     Returns
     -------
     set[str]
-        Contributors of file
+        Contributors of source
     """
     contributors = set()
     commits = repo.get_commits(path=filepath)
@@ -34,12 +40,14 @@ def query_contributors(repo: Repository, filepath: str) -> set[str]:
 
 
 def create_github_issue(
-    imagery_filepath: str, imagery_name: str, reason: str, days: int
+    repo: Repository, imagery_filepath: str, imagery_name: str, reason: str, days: int
 ) -> None:
     """Create new github issue
 
     Parameters
     ----------
+    repo : Repository
+        The repository to query
     imagery_filepath : str
         The path to the file the issue is created for
     imagery_name : str
@@ -51,10 +59,6 @@ def create_github_issue(
     """
 
     try:
-        # Github Settings -> Developer Settings -> Personal access tokens -> Enable repo / public_repo scope
-        pa_token = os.environ["PA_TOKEN"]
-        g = Github(pa_token)
-        repo = g.get_repo(GITHUB_REPO)
 
         contributors = query_contributors(repo, imagery_filepath)
 
@@ -70,19 +74,71 @@ def create_github_issue(
             f"{', '.join([f'@{contributor}' for contributor in contributors])}",
         ]
         body = "\n".join(body_lines)
-
-        repo.create_issue(title=title, body=body)
+        repo.create_issue(title=title, body=body, labels=["imagery"])
     except Exception as e:
         print(f"Failed to create issue: {e}")
+
+
+def close_github_issue(repo: Repository, issue: Issue) -> None:
+    """Close a github issue
+
+    Parameters
+    ----------
+    repo : Repository
+        The repository
+    issue : Issue
+        The issue
+    """
+    try:
+        issue.create_comment(f"Imagery seems to work again.")
+        issue.edit(state=ISSUE_CLOSED)
+    except Exception as e:
+        print(f"Failed to close issue: {e}")
+
+
+def get_watchdog_issues(repo: Repository) -> dict[str, Issue]:
+    """Query repository for watchdog issues
+
+    Parameters
+    ----------
+    repo : Repository
+        The repository to query
+
+    Returns
+    -------
+    dict[str, Issue]
+        The issues
+    """
+    watchdog_issues = {}
+    issues = repo.get_issues(state="all", creator="rbuffat")
+    for issue in issues:
+        if "[Watchdog]" in issue.title:
+            imagery_path = re.search(r"sources(.*?)geojson", issue.title).group(0)
+            watchdog_issues[imagery_path] = issue
+    return watchdog_issues
 
 
 def notify_broken_imagery(data):
     broken_sources_db = "web/broken.json"
 
+    # Github Settings -> Developer Settings -> Personal access tokens -> Enable repo / public_repo scope
+    pa_token = os.environ["PA_TOKEN"]
+    g = Github(pa_token)
+    repo = g.get_repo(GITHUB_REPO)
+
     if os.path.exists(broken_sources_db):
         with open(broken_sources_db) as f:
             broken = json.load(f)
 
+        # Get existing watchdog issues (open or closed)
+        watchdog_issues = get_watchdog_issues(repo)
+        open_watchdog_issues = {
+            key: value
+            for (key, value) in watchdog_issues.items()
+            if value.state == ISSUE_OPEN
+        }
+
+        broken_imagery_paths = set()
         for d in data:
             if not d["imagery"]["status"] == "error":
                 continue
@@ -97,7 +153,6 @@ def notify_broken_imagery(data):
             days = (datetime.date.today() - broken_date).days + 1
 
             print(f"Imagery broken: {d['name']}: Days {days}")
-
             if days == CREATE_ISSUE_AFTER_DAYS:
                 print(f"Imagery broken: Notify!")
                 reason = "\n".join(
@@ -108,4 +163,11 @@ def notify_broken_imagery(data):
                     ]
                 )
                 filepath = f"sources/{'/'.join(d['directory'])}/{d['filename']}"
-                create_github_issue(filepath, imagery_name, reason, days)
+                broken_imagery_paths.add(filepath)
+                # TODO reopen existing issue instead of creating a new one
+                create_github_issue(repo, filepath, imagery_name, reason, days)
+
+        # Close open issues for sources that aren't broken anymore
+        for filepath, watchdog_issue in open_watchdog_issues.items():
+            if filepath not in broken_imagery_paths:
+                close_github_issue(repo, watchdog_issue)
